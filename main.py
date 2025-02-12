452181dd05ca4d9756a650657d0f5308904c268e
wj-jung <101616239+wj-jung@users.noreply.github.com>
2025-02-12 14:24:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def get_project_num(org_id):
         get_cloudsql_version(project_name_split(response.project))
 
 def project_name_split(project):
-    return project.split('/',1)[1] # projects/12345678 to 12345678
+    return project.split('/',1)[1] # project/12345678 to 12345678
 
 def get_cloudsql_version(project_id):
     credentials = GoogleCredentials.get_application_default()
@@ -34,31 +34,32 @@ def get_cloudsql_version(project_id):
     while request is not None:
         try:
             response = request.execute()
+
+            if len(response) == 0:
+                break
+
+            for database_instance in response['items']:
+                for instance_version in instance_versions:
+                    if database_instance["databaseVersion"] == instance_version:
+                        result = {
+                            "project_num": project,
+                            "project": database_instance["project"],
+                            "displayName": database_instance["name"],
+                            "databaseVersion": database_instance["databaseVersion"],
+                            "gceZone": database_instance["gceZone"]
+                        }
+                        results.append(result)
+            request = service.instances().list_next(previous_request=request, previous_response=response)
+            
         except Exception as e:
             exception_http = f"{e}"
             if "HttpError 403 when requesting" in exception_http:
                 break
 
             print(f"Project : {project}")
-            pprint(f"Error : {e}")
+            print(f"Error : {e}")
             print("-" * 60)
             break
-
-        if len(response) == 0:
-            break
-
-        for database_instance in response['items']:
-            for instance_version in instance_versions:
-                if database_instance["databaseVersion"] == instance_version:
-                    result = {
-                        "project_num": project,
-                        "project": database_instance["project"],
-                        "displayName": database_instance["name"],
-                        "databaseVersion": database_instance["databaseVersion"],
-                        "gceZone": database_instance["gceZone"]
-                    }
-                    results.append(result)
-        request = service.instances().list_next(previous_request=request, previous_response=response)
 
     if len(results) != 0:
         pprint(results)
